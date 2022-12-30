508cd00792fb65fcc6350dd4f8d6166d6fcb6d53
mudasir <96320211+mud-ali@users.noreply.github.com>
2022-12-29 21:33:06 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './css/App.css';
+import React from 'react';
+import './css/index.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Navbar />
+    </div>
   );
 }
 
