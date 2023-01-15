56453d9038ef1ea0dfb5492af074f002e5451794
mudasir <96320211+mud-ali@users.noreply.github.com>
2023-01-15 18:19:25 -0500

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,43 +1,48 @@
 import React from 'react';
 import '../css/index.css';
 import Navbar from '../components/Navbar';
-import Bigsection from '../components/Bigsection';
 import Footer from '../components/Footer';
+import Cardsection from '../components/Cardsection';
 
 function Contact() {
     return (
         <div className="App">
             <Navbar />
-            <Bigsection
-                title="Contact Us"
-                subtitle="If you have any questions, please contact us at:"
-                content=""
+            <h1>Contact Us</h1>
+            <h5>If you have any questions or concerns, or are interested in becoming a sponsor, please contact us at:</h5>
+            <Cardsection
                 settings={{
                     side: "left",
                     subtitleSize: "1.5rem"
                 }}
                 cards={[
-                    {
+                    {  
                         title: "Email",
                         image: null,
-                        content: "",
+                        content: <span>
+                                    <p>You can send us an email at:</p>
+                                    <i className="fa-regular fa-envelope fa-7x"></i>
+                                </span>,
                         link: {
                             url: "mailto:titaniumknightsbca@gmail.com",
                             text: "titaniumknightsbca@gmail.com"
                         }
                     },
-                    /*{
-                        title: "Discord",
+                    {
+                        title: "Instagram",
                         image: null,
-                        content: "Send us a DM @",
+                        content: <span>
+                                    <p>Send us a DM @</p>
+                                    <i className='fa-brands fa-instagram fa-7x'></i>
+                                </span>,
                         link: {
-                            url: "https://discordapp.com/users/668984210777833484",
-                            text: "titaniumknightsrobotics#1007"
+                            url: "https://www.instagram.com/titaniumknightsftc/",
+                            text: "titaniumknightsftc"
                         },
                         settings: {
                             forceColor: "white"
                         }
-                    }*/
+                    }
                 ]}
             />
             <Footer />
